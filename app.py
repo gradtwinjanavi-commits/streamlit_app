a60eefab38692c05e35b09d0d3dd0e75acf4ccc7
gradtwinjanavi-commits <gradtwin.janavi@gmail.com>
2026-01-29 12:01:48 +0530

    Initialize Streamlit app with welcome message

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome DS Interns",
+    page_icon="🤖",
+    layout="centered"
+)
+
+st.title("Hi Sri Harieesh")
+st.subheader("Welcome my dear DS Interns 🚀")
+st.subheader("Learn Streamlit")
+st.subheader("Python Framework")
